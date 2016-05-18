130dc241032d2c354630f46b24e920520f2d3251
Aitor Oses <aitor.oses@gmail.com>
2016-05-18 16:55:59 +0200

    ELK stack is configured to run, cadvisor emits it's results to elasticsearch

--- discovery/reload-haproxy.sh
+++ discovery/reload-haproxy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 /usr/local/sbin/haproxy -f /etc/haproxy.cfg -p /var/run/haproxy.pid -sf $(cat /var/run/haproxy.pid)
+
+# Rsyslog (Apparently needed for taking the config)
+/etc/init.d/rsyslog restart
